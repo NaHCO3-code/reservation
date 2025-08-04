8a8322eeb6800f41f2c6f9a7d385f85a56c751f7
surfish <shrshr2@163.com>
2025-08-04 11:40:14 +0800

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  build: {
-    outDir: 'docs',
-
-  },
-  base: '/reservation/'
-})
+  base: "/",
+});
